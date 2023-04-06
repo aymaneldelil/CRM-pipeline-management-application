2eeeb1323430d2218c3da93bf5e0e00c8b5cc7f4
aymanyori <aymanyori00@gmail.com>
2023-04-06 16:58:44 +0200

    MajorTabs

--- src/app/features/pipeline-feature/services/pipeline.service.ts
+++ src/app/features/pipeline-feature/services/pipeline.service.ts
@@ -1,7 +1,59 @@
 import { Injectable } from '@angular/core';
+import { PipelineTabs } from '../interfaces/pipeline-tabs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ideals } from '../interfaces/ideals';
 
 @Injectable()
 export class PipelineService {
+  public readonly mockMajorTabs: Array<PipelineTabs> = [
+    {
+      name: 'Exhibitors',
+      icon: 'storefront',
+      path: 'exhibitors',
+    },
+    {
+      name: 'Sales',
+      icon: 'local_mall',
+      path: 'sales',
+      nestedtabs: [
+        {
+          name: 'Deals',
+          path: '/deals',
+        },
+        {
+          name: 'Activites',
+          path: '/activites',
+        },
+        {
+          name: 'Statistics',
+          path: '/statistics',
+        },
+      ],
+    },
+    {
+      name: 'Program',
+      icon: 'calendar_today',
+      path: 'program',
+    },
+    {
+      name: 'Delegates',
+      icon: 'perm_contact_calendar',
+      path: 'delegates',
+    },
+    {
+      name: 'Settings',
+      icon: 'manage_accounts',
+      path: 'settings',
+    },
+  ];
+  //---------------------------------------------------------------------------------------------------------------------------------------------------
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  //---------------------------------------------------------------------------------------------------------------------------------------------------
+  public getDeals(): Observable<Ideals> {
+    return this.http.get<Ideals>(
+      'https://my-json-server.typicode.com/mabukoush1/contacts/db'
+    );
+  }
 }
