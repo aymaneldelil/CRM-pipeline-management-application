f3e8ab567330bc2a44ca19f94694a2f2f394d98f
aymanyori <aymanyori00@gmail.com>
2023-04-06 17:28:21 +0200

    Install Angular CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HNavbarModule } from './features/h-navbar/h-navbar.module';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HNavbarModule
+    HNavbarModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
