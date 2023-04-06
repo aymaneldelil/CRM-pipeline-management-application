79c59d0abd7ede3f3886cd3c26998057bb18ae43
aymanyori <aymanyori00@gmail.com>
2023-04-06 17:28:32 +0200

    Angular CDK

--- src/app/features/pipeline-feature/pipeline-feature.module.ts
+++ src/app/features/pipeline-feature/pipeline-feature.module.ts
@@ -5,6 +5,7 @@ import { MajorTabsComponent } from './components/pipeline-major-tabs/major-tabs.
 import { PipelineService } from './services/pipeline.service';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [MinorTabsComponent, MajorTabsComponent],
   imports: [CommonModule, AngularMaterialModule, RouterModule],
